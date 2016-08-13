7e43ab5f98c3486a5775df0a325ed450f503f7b2
Danilo Lima <danilo.lima@wimdu.com>
2016-08-14 00:15:17 +0200

    add validation for input

--- kata.rb
+++ kata.rb
@@ -2,15 +2,19 @@ class Kata
   attr_reader :input
 
   def initialize(input)
-    @input = input.upcase
+    @input = input
   end
 
   def run
-    map_letters = ("A"..input).to_a
-    lines = map_letters.each_with_index.map do |letter, index|
-      build_line(letter, index, map_letters.size)
+    if valid_input?
+      map_letters = ("A"..input.upcase).to_a
+      lines = map_letters.each_with_index.map do |letter, index|
+        build_line(letter, index, map_letters.size)
+      end
+      (lines + lines.reverse[1..-1]).join "\n"
+    else
+      "your input is invalid for this kata!"
     end
-    (lines + lines.reverse[1..-1]).join "\n"
   end
 
   private
@@ -23,6 +27,12 @@ class Kata
 
   def gap(index)
     index * 2 - 1
+  end
+
+  def valid_input?
+    return false unless input.is_a?(String)
+    return false unless ("a".."z").include?(input)
+    true
   end
 end
 
--- spec.rb
+++ spec.rb
@@ -5,41 +5,55 @@ require "minitest/spec"
 class KataSpec < Minitest::Spec
   let(:result) { Kata.new(input).run }
 
-  describe "a" do
-    let(:input) { "a" }
+  describe "#run" do
+    describe "invalid input" do
+      it "returns a message" do
+        invalid_message = "your input is invalid for this kata!"
 
-    it "returns upcase 'a'" do
-      assert_equal("A", result)
+        assert_equal(invalid_message, Kata.new(123).run)
+        assert_equal(invalid_message, Kata.new("123").run)
+        assert_equal(invalid_message, Kata.new(false).run)
+        assert_equal(invalid_message, Kata.new("!!!").run)
+        assert_equal(invalid_message, Kata.new("Ç€^><ßÄ").run)
+      end
     end
-  end
 
-  describe "b" do
-    let(:input) { "b" }
+    describe  "a" do
+      let(:input) { "a" }
 
-    it "returns 'ab' diamond" do
-      expected_output = " A\nB B\n A"
-
-      assert_equal(expected_output, result)
+      it "returns 'A'" do
+        assert_equal("A", result)
+      end
     end
-  end
 
-  describe "c" do
-    let(:input) { "c" }
+    describe "b" do
+      let(:input) { "b" }
 
-    it "returns 'abc' diamond" do
-      expected_output = "  A\n B B\nC   C\n B B\n  A"
+      it "returns 'AB' diamond" do
+        expected_output = " A\nB B\n A"
 
-      assert_equal(expected_output, result)
+        assert_equal(expected_output, result)
+      end
     end
-  end
 
-  describe "d" do
-    let(:input) { "d" }
+    describe "c" do
+      let(:input) { "c" }
 
-    it "returns 'abcd' diamond" do
-      expected_output = "   A\n  B B\n C   C\nD     D\n C   C\n  B B\n   A"
+      it "returns 'ABC' diamond" do
+        expected_output = "  A\n B B\nC   C\n B B\n  A"
 
-      assert_equal(expected_output, result)
+        assert_equal(expected_output, result)
+      end
+    end
+
+    describe "d" do
+      let(:input) { "d" }
+
+      it "returns 'ABCD' diamond" do
+        expected_output = "   A\n  B B\n C   C\nD     D\n C   C\n  B B\n   A"
+
+        assert_equal(expected_output, result)
+      end
     end
   end
 end
