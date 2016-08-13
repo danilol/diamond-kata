572525de8865f73aa9663cae2e284a5bc049c73f
Danilo Lima <danilo.lima@wimdu.com>
2016-08-13 23:32:35 +0200

    First release - working prototype

--- kata.rb
+++ kata.rb
@@ -2,10 +2,27 @@ class Kata
   attr_reader :input
 
   def initialize(input)
-    @input = input
+    @input = input.upcase
   end
 
   def run
-    input
+    map_letters = ("A"..input).to_a
+    lines = map_letters.each_with_index.map do |letter, index|
+      build_line(letter, index, map_letters.size)
+    end
+    (lines + lines.reverse[1..-1]).join "\n"
+  end
+
+  private
+
+  def build_line(letter, index, size)
+    space = size - index - 1
+    return " " * space + letter if index.zero?
+    " " * space + letter + " " * gap(index) + letter
+  end
+
+  def gap(index)
+    index * 2 - 1
   end
 end
+
--- spec.rb
+++ spec.rb
@@ -1,16 +1,45 @@
-require_relative 'kata'
-require 'minitest/autorun'
-require 'minitest/spec'
+require_relative "kata"
+require "minitest/autorun"
+require "minitest/spec"
 
 class KataSpec < Minitest::Spec
-  let(:input) { '' }
   let(:result) { Kata.new(input).run }
 
-  describe 'a' do
-    let(:input) { 'a' }
+  describe "a" do
+    let(:input) { "a" }
 
-    it 'returns "a"' do
-      assert_equal "a", result
+    it "returns upcase 'a'" do
+      assert_equal("A", result)
+    end
+  end
+
+  describe "b" do
+    let(:input) { "b" }
+
+    it "returns 'ab' diamond" do
+      expected_output = " A\nB B\n A"
+
+      assert_equal(expected_output, result)
+    end
+  end
+
+  describe "c" do
+    let(:input) { "c" }
+
+    it "returns 'abc' diamond" do
+      expected_output = "  A\n B B\nC   C\n B B\n  A"
+
+      assert_equal(expected_output, result)
+    end
+  end
+
+  describe "d" do
+    let(:input) { "d" }
+
+    it "returns 'abcd' diamond" do
+      expected_output = "   A\n  B B\n C   C\nD     D\n C   C\n  B B\n   A"
+
+      assert_equal(expected_output, result)
     end
   end
 end
