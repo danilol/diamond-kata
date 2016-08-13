5ed36331e0646354766ee09cfdf6671b9960c01e
Danilo Lima <danilo.lima@wimdu.com>
2016-08-14 00:54:48 +0200

    refactoring run method

--- kata.rb
+++ kata.rb
@@ -1,28 +1,29 @@
 class Kata
   attr_reader :input
 
+  SEPARATOR = " "
+  BASE      = "A"
+
   def initialize(input)
     @input = input
   end
 
   def run
-    if valid_input?
-      map_letters = ("A"..input.upcase).to_a
-      lines = map_letters.each_with_index.map do |letter, index|
-        build_line(letter, index, map_letters.size)
-      end
-      (lines + lines.reverse[1..-1]).join "\n"
-    else
-      "your input is invalid for this kata!"
+    return   "your input is invalid for this kata!" unless valid_input?
+
+    lines = diamond_chars.each_with_index.map do |letter, index|
+      build_line(letter, index, diamond_chars.size)
     end
+
+    (lines + lines.reverse[1..-1]).join "\n"
   end
 
   private
 
   def build_line(letter, index, size)
     space = size - index - 1
-    return " " * space + letter if index.zero?
-    " " * space + letter + " " * gap(index) + letter
+    return SEPARATOR * space + letter if index.zero?
+    SEPARATOR * space + letter + SEPARATOR * gap(index) + letter
   end
 
   def gap(index)
@@ -33,6 +34,10 @@ class Kata
     return false unless input.is_a?(String)
     return false unless ("a".."z").include?(input)
     true
+  end
+
+  def diamond_chars
+    @diamond_chars ||= ("A"..input.upcase).to_a
   end
 end
 
