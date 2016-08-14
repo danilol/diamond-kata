738a3f569978246b705f0687ef26ffa6183fb9b4
Danilo Lima <danilo.lima@wimdu.com>
2016-08-14 17:07:09 +0200

    changing size logi

--- kata.rb
+++ kata.rb
@@ -12,7 +12,7 @@ class Kata
     return "your input is invalid for this kata!" unless valid_input?
 
     lines = diamond_chars.each_with_index.map do |letter, index|
-      build_line(letter, index, diamond_chars.size)
+      build_line(letter, index)
     end
 
     (lines + lines.reverse[1..-1]).join "\n"
@@ -20,8 +20,8 @@ class Kata
 
   private
 
-  def build_line(letter, index, size)
-    space = size - index - 1
+  def build_line(letter, index)
+    space = size - index
     return SEPARATOR * space + letter + SEPARATOR * space if first_char?(letter)
     SEPARATOR * space + letter + SEPARATOR * gap(index) + letter + SEPARATOR * space
   end
@@ -32,6 +32,10 @@ class Kata
 
   def gap(index)
     index * 2 - 1
+  end
+
+  def size
+    input.upcase.ord - BASE.ord
   end
 
   def valid_input?
